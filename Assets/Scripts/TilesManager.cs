6673c467cbe4775c765353a244539f5393d3e010
Elliot Kauffmann <elliot.kauffmann@gmail.com>
2023-09-30 17:21:38 +0200

    Tile below

--- Assets/Scripts/TilesManager.cs
+++ Assets/Scripts/TilesManager.cs
@@ -4,79 +4,76 @@ using UnityEngine;
 
 public class TilesManager : MonoBehaviour
 {
-    public GameObject tilePrefab;
+  public GameObject tilePrefab;
 
-    public int width = 7;
-    public float tileWidth = 4;
+  public int width = 7;
+  public float tileWidth = 4;
 
-    private List<GameObject> Tiles;
+  private List<GameObject> Tiles;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    // initialize Tiles 
+    Tiles = new List<GameObject>(width * width);
+    // create tiles
+    for (int i = 0; i < width; i++)
     {
-        // initialize Tiles 
-        Tiles = new List<GameObject>(width * width);
-        // create tiles
-        for(int i = 0; i < width; i++)
+      for (int j = 0; j < width; j++)
+      {
+        // create tile
+        GameObject tile = Instantiate(tilePrefab, new Vector3(i * tileWidth, j * tileWidth, 1), Quaternion.identity, transform);
+        Tiles.Add(tile);
+
+        // in the corners of the map, there are no tiles
+        if (noTiles(i, j))
         {
-          for(int j = 0; j < width; j++)
-          {
-            // create tile
-            GameObject tile = Instantiate(tilePrefab);
-            tile.transform.position = new Vector3(i * tileWidth, j * tileWidth, 0);
-            tile.transform.parent = transform;
-            tile.transform.localScale = new Vector3(tileWidth * 0.97f, tileWidth * 0.97f, 1);
-            Tiles.Add(tile);
-
-            // in the corners of the map, there are no tiles
-            if(noTiles(i, j))
-            {
-              tile.GetComponent<SpriteRenderer>().color = Color.black;
-              tile.SetActive(false);
-            }
-          }
+          tile.GetComponent<SpriteRenderer>().color = Color.black;
+          tile.SetActive(false);
         }
-        
+      }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 
-    public int getTile(int x, int y)
-    {
-      if(noTiles(x, y))
-      {
-        return -1;
-      }
-      if(x < 0 || x >= width || y < 0 || y >= width)
-      {
-        return -1;
-      }
-      return x * width + y;
-    }
+  // Update is called once per frame
+  void Update()
+  {
 
-    private bool noTiles(int x, int y)
+  }
+
+  public int getTile(int x, int y)
+  {
+    if (noTiles(x, y))
     {
-      // no corner tiles
-      if(x == 0 && y == 0)
-      {
-        return true;
-      }
-      if(x == 0 && y == width - 1)
-      {
-        return true;
-      }
-      if(x == width - 1 && y == 0)
-      {
-        return true;
-      }
-      if(x == width - 1 && y == width - 1)
-      {
-        return true;
-      }
-      return false;
+      return -1;
     }
+    if (x < 0 || x >= width || y < 0 || y >= width)
+    {
+      return -1;
+    }
+    return x * width + y;
+  }
+
+  private bool noTiles(int x, int y)
+  {
+    // no corner tiles
+    if (x == 0 && y == 0)
+    {
+      return true;
+    }
+    if (x == 0 && y == width - 1)
+    {
+      return true;
+    }
+    if (x == width - 1 && y == 0)
+    {
+      return true;
+    }
+    if (x == width - 1 && y == width - 1)
+    {
+      return true;
+    }
+    return false;
+  }
 }
