334a7cb1625cf0ae09068877f5702aeabd24e517
Théotime Eliet <theo.eliet@gmail.com>
2023-09-30 22:22:51 +0200

    NextWaveButton hover

---
+++ Assets/Scripts/onHoverButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onHover : MonoBehaviour
+{
+
+    public GameObject normalDisplay;
+    public GameObject hoverDisplay;
+
+    void Start()
+    {
+        normalDisplay.SetActive(true);
+        hoverDisplay.SetActive(false);
+    }
+
+    public void OnHoverEnter()
+    {
+        normalDisplay.SetActive(false);
+        hoverDisplay.SetActive(true);
+    }
+
+    public void OnHoverExit()
+    {
+        normalDisplay.SetActive(true);
+        hoverDisplay.SetActive(false);
+    }
+}
