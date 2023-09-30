ea058231b411187b65322de9d7a1c965cf187dd5
Théotime Eliet <theo.eliet@gmail.com>
2023-10-01 00:29:10 +0200

    Not walkable overlay

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     private bool isWalkable = false;
+    private GameObject notPosessedOverlay;
+    private GameObject gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        notPosessedOverlay = transform.GetChild(0).gameObject;
+        if(isWalkable)
+        {
+          notPosessedOverlay.SetActive(false);
+        }
+        else
+        {
+          notPosessedOverlay.SetActive(true);
+        }
     }
 
     // Update is called once per frame
@@ -25,10 +35,12 @@ public class Tile : MonoBehaviour
       if(isWalkable)
       {
         GetComponent<SpriteRenderer>().color = Color.white;
+        notPosessedOverlay.SetActive(false);
       }
       else
       {
         GetComponent<SpriteRenderer>().color = Color.black;
+        notPosessedOverlay.SetActive(true);
       }
     }
 
