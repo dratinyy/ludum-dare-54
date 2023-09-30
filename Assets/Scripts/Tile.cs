a389f01260347c42a86f8ef8acecd912c902e09c
Théotime Eliet <theo.eliet@gmail.com>
2023-10-01 01:25:05 +0200

    added Tile states

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     private bool isWalkable = false;
-    private GameObject notPosessedOverlay;
+
+    public enum State 
+    {
+        notOwned, 
+        Owned,
+        Rented
+    };
+
+    // Colors 
+    public Color notOwnedColor;
+    // public Color SelectedColor;
+    public Color RentedColor;
+
+    private State currentState = State.notOwned;
+
+    public GameObject overlay;
+
+    public GameObject rentedOverlay;
     private GameObject gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        notPosessedOverlay = transform.GetChild(0).gameObject;
+        // Colors 
+        notOwnedColor = overlay.GetComponent<SpriteRenderer>().color;
+        // Gold color with 50% alpha
+        RentedColor = new Color(1, 0.92f, 0.016f, 0.5f);
+
         if(isWalkable)
         {
-          notPosessedOverlay.SetActive(false);
+          overlay.SetActive(false);
         }
         else
         {
-          notPosessedOverlay.SetActive(true);
+          overlay.SetActive(true);
         }
     }
 
@@ -35,12 +56,12 @@ public class Tile : MonoBehaviour
       if(isWalkable)
       {
         GetComponent<SpriteRenderer>().color = Color.white;
-        notPosessedOverlay.SetActive(false);
+        overlay.SetActive(false);
       }
       else
       {
         GetComponent<SpriteRenderer>().color = Color.black;
-        notPosessedOverlay.SetActive(true);
+        overlay.SetActive(true);
       }
     }
 
@@ -48,4 +69,49 @@ public class Tile : MonoBehaviour
     {
       return isWalkable;
     }
+
+    public void setState(State newState)
+    {
+        currentState = newState;
+        switch(newState)
+        {
+            case State.notOwned:
+            overlay.SetActive(true);
+            rentedOverlay.SetActive(false);
+            overlay.GetComponent<SpriteRenderer>().color = notOwnedColor;
+            setIsWalkable(false);
+            break;
+            case State.Owned:
+            overlay.SetActive(false);
+            rentedOverlay.SetActive(false);
+            setIsWalkable(true);
+            break;
+            case State.Rented:
+            overlay.SetActive(true);
+            rentedOverlay.SetActive(true);
+            overlay.GetComponent<SpriteRenderer>().color = RentedColor;
+            setIsWalkable(false);
+            break;
+        }
+    }
+  void OnMouseOver()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+        print("clicked");
+        // carrousel states 
+        if (currentState == State.notOwned)
+        {
+            setState(State.Owned);
+        }
+        else if (currentState == State.Owned)
+        {
+            setState(State.Rented);
+        }
+        else if (currentState == State.Rented)
+        {
+            setState(State.notOwned);
+        }
+    }
+  }
 }
