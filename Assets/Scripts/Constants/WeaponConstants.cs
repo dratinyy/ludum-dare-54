c5d2f2eae420b8f079f48ac05ccd742c0fac021b
Elliot Kauffmann <elliot.kauffmann@gmail.com>
2023-10-01 04:10:53 +0200

    shoot

--- Assets/Scripts/Constants/WeaponConstants.cs
+++ Assets/Scripts/Constants/WeaponConstants.cs
@@ -7,14 +7,51 @@ public class WeaponConstants : MonoBehaviour
 
     public static readonly WeaponStats[] weaponStats = new WeaponStats[]
     {
-        // Weapon 0
+        // Weapon 0 Gun
         new WeaponStats
         {
             projectileSpeed = 8f,
-            damage = 10f,
-            attackSpeed = 6f,
-            range = 3f
-        }
+            damage = 7f,
+            attackSpeed = 5f,
+            range = 3f,
+            randomAngle = 0.05f
+        },
+        // Weapon 1 Shotgun
+        new WeaponStats
+        {
+            projectileSpeed = 8f,
+            damage = 12f,
+            attackSpeed = 3f,
+            range = 1.5f,
+            randomAngle = 0.25f
+        },
+        // Weapon 2 Auto Rifle
+        new WeaponStats
+        {
+            projectileSpeed = 16f,
+            damage = 4f,
+            attackSpeed = 20f,
+            range = 4.5f,
+            randomAngle = 0.25f
+        },
+        // Weapon 3 Sniper
+        new WeaponStats
+        {
+            projectileSpeed = 12f,
+            damage = 8f,
+            attackSpeed = 7.5f,
+            range = 6f,
+            randomAngle = 0.25f
+        },
+        // Weapon 4 Rocket Launcher
+        new WeaponStats
+        {
+            projectileSpeed = 4f,
+            damage = 30f,
+            attackSpeed = 1.67f,
+            range = 9f,
+            randomAngle = 0.25f
+        },
     };
 
     void Start()
@@ -32,5 +69,6 @@ public class WeaponConstants : MonoBehaviour
         public float damage;
         public float attackSpeed;
         public float range;
+        public float randomAngle;
     }
 }
