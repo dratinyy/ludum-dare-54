26d53ca32af15df134b2226624c635f356e08cc9
Théotime Eliet <theo.eliet@gmail.com>
2023-10-01 19:00:44 +0200

    get economy constants

--- Assets/Scripts/TileMenuButton.cs
+++ Assets/Scripts/TileMenuButton.cs
@@ -9,7 +9,26 @@ public class TileMenuButton : MonoBehaviour
     public GameObject hoverDisplay;
     public GameObject Tile;
 
+    public GameObject priceText;
+
     public string type;
+
+    void Start()
+    {
+        // Set price text depending on type and get price in EconomyConstants
+        if (type == "rent")
+        {
+            priceText.GetComponent<UnityEngine.UI.Text>().text = EconomyConstants.tileRentBenefit.ToString();
+        }
+        else if (type == "buy")
+        {
+            priceText.GetComponent<UnityEngine.UI.Text>().text = EconomyConstants.tileBuyPrice.ToString();
+        }
+        else if (type == "sell")
+        {
+            priceText.GetComponent<UnityEngine.UI.Text>().text = EconomyConstants.tileSellPrice.ToString();
+        }
+    }
 
     void OnMouseOver()
     {
